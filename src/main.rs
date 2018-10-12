eb1535476a9bd6e11a93bcaadbacf600b0d47a73
Sebastian Dröge <sebastian@centricular.com>
2018-10-12 13:35:28 +0300

    Add first step of GStreamer integration
    This simply shows the camera in the UI and handles errors that might
    happen during capturing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 extern crate gio;
+extern crate glib;
+extern crate gstreamer as gst;
 extern crate gtk;
+
+extern crate fragile;
 
 use gio::prelude::*;
 use gio::MenuExt;
 use gtk::prelude::*;
 
+use gst::prelude::*;
+use gst::BinExt;
+
 use std::cell::RefCell;
 use std::env::args;
+use std::error;
 use std::rc::{Rc, Weak};
 
 macro_rules! upgrade_weak {
@@ -29,8 +37,13 @@ struct App(Rc<RefCell<AppInner>>);
 struct AppWeak(Weak<RefCell<AppInner>>);
 
 impl App {
-    fn new() -> App {
-        App(Rc::new(RefCell::new(AppInner { main_window: None })))
+    fn new(application: &gtk::Application) -> App {
+        App(Rc::new(RefCell::new(AppInner {
+            application: application.clone(),
+            main_window: None,
+            pipeline: None,
+            error: None,
+        })))
     }
 
     fn downgrade(&self) -> AppWeak {
@@ -45,7 +58,10 @@ impl AppWeak {
 }
 
 struct AppInner {
+    application: gtk::Application,
     main_window: Option<gtk::ApplicationWindow>,
+    pipeline: Option<gst::Pipeline>,
+    error: Option<Box<dyn error::Error>>,
 }
 
 fn build_actions(_app: &App, application: &gtk::Application) {
@@ -88,6 +104,67 @@ fn build_actions(_app: &App, application: &gtk::Application) {
     application.add_action(&about);
 }
 
+fn create_pipeline(app: &App) -> Result<(gst::Pipeline, gtk::Widget), Box<dyn error::Error>> {
+    // Create a new GStreamer pipeline that captures from the default video source,
+    // which is usually a camera, converts the output to RGB if needed and then passes
+    // it to a GTK video sink
+    let pipeline = gst::parse_launch("autovideosrc ! queue ! videoconvert ! gtksink name=sink")?;
+
+    // Upcast to a gst::Pipeline as the above function could've also returned
+    // an arbitrary gst::Element if a different string was passed
+    let pipeline = pipeline.downcast::<gst::Pipeline>().unwrap();
+
+    // Install a message handler on the pipeline's bus to catch errors
+    let bus = pipeline.get_bus().unwrap();
+
+    // GStreamer is thread-safe and it is possible to attach
+    // bus watches from any thread, which are then nonetheless
+    // called from the main thread. As such we have to make use
+    // of fragile::Fragile() here to be able to pass our non-Send
+    // application struct into a closure that requires Send.
+    //
+    // As we are on the main thread and the closure will be called
+    // on the main thread, this will not cause a panic and is perfectly
+    // safe.
+    let app_weak = fragile::Fragile::new(app.downgrade());
+    bus.add_watch(move |_bus, msg| {
+        use gst::MessageView;
+
+        let app_weak = app_weak.get();
+        let app = upgrade_weak!(app_weak, glib::Continue(false));
+
+        // A message can contain various kinds of information but
+        // here we are only interested in errors so far
+        match msg.view() {
+            MessageView::Error(err) => {
+                eprintln!(
+                    "Error from {:?}: {} ({:?})",
+                    err.get_src().map(|s| s.get_path_string()),
+                    err.get_error(),
+                    err.get_debug()
+                );
+
+                // On errors, we store the error that happened
+                // and print it later
+                let mut inner = app.0.borrow_mut();
+
+                inner.error = Some(Box::new(err.get_error()));
+                inner.application.quit();
+            }
+            _ => (),
+        };
+
+        glib::Continue(true)
+    });
+
+    // Get the GTK video sink and retrieve the video display widget from it
+    let sink = pipeline.get_by_name("sink").unwrap();
+    let widget_value = sink.get_property("widget").unwrap();
+    let widget = widget_value.get::<gtk::Widget>().unwrap();
+
+    Ok((pipeline, widget))
+}
+
 fn build_ui(app: &App, application: &gtk::Application) {
     let window = gtk::ApplicationWindow::new(application);
     app.0.borrow_mut().main_window = Some(window.clone());
@@ -122,13 +199,34 @@ fn build_ui(app: &App, application: &gtk::Application) {
 
     header_bar.pack_end(&main_menu);
     window.set_titlebar(&header_bar);
+
+    // Create the pipeline and if that fails, shut down and
+    // remember the error that happened
+    let (pipeline, view) = match create_pipeline(app) {
+        Err(err) => {
+            app.0.borrow_mut().error = Some(err);
+            application.quit();
+            return;
+        }
+        Ok(res) => res,
+    };
+
+    // Store the pipeline for later usage and add the view widget
+    // to the UI
+    app.0.borrow_mut().pipeline = Some(pipeline);
+
+    // A Box allows to place multiple widgets next to each other
+    // vertically or horizontally
+    let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
+    vbox.pack_start(&view, true, true, 0);
+    window.add(&vbox);
 }
 
-fn main() {
-    let app = App::new();
+fn main() -> Result<(), Box<dyn error::Error>> {
+    gst::init()?;
+    let application = gtk::Application::new("com.github.rustfest", gio::ApplicationFlags::empty())?;
 
-    let application = gtk::Application::new("com.github.rustfest", gio::ApplicationFlags::empty())
-        .expect("Initialization failed...");
+    let app = App::new(&application);
 
     // On application startup (of the main instance) we create
     // the actions and UI. A second process would not run this
@@ -144,9 +242,9 @@ fn main() {
     // when the first process is started, and in the first process
     // whenever a second process is started
     let app_weak = app.downgrade();
-    application.connect_activate(move |_| {
+    application.connect_activate(move |application| {
         let app = upgrade_weak!(app_weak);
-        let inner = app.0.borrow();
+        let mut inner = app.0.borrow_mut();
         // We only show our window here once the application
         // is activated. This means that when a second instance
         // is started, the window of the first instance will be
@@ -155,14 +253,45 @@ fn main() {
             main_window.show_all();
             main_window.present();
         }
+
+        // Once the UI is shown, start the GStreamer pipeline. If
+        // an error happens, we immediately shut down
+        {
+            // Needed because we need to borrow two parts of the application struct
+            let AppInner {
+                ref pipeline,
+                ref mut error,
+                ..
+            } = *inner;
+
+            if let Some(ref pipeline) = pipeline {
+                if let Err(err) = pipeline.set_state(gst::State::Playing).into_result() {
+                    *error = Some(Box::new(err));
+                    application.quit();
+                }
+            }
+        }
     });
 
-    // This takes ownership of our App struct so it stays
-    // alive as long as the application does
+    // When the application is shut down, first shut down
+    // the GStreamer pipeline so that capturing can gracefully stop
+    let app_weak = app.downgrade();
     application.connect_shutdown(move |_| {
-        let _ = app;
+        let app = upgrade_weak!(app_weak);
+        let inner = app.0.borrow();
+
+        if let Some(ref pipeline) = inner.pipeline {
+            let _ = pipeline.set_state(gst::State::Null);
+        }
     });
 
     // And now run the application until the end
     application.run(&args().collect::<Vec<_>>());
+
+    let mut app_inner = app.0.borrow_mut();
+    if let Some(err) = app_inner.error.take() {
+        Err(err)
+    } else {
+        Ok(())
+    }
 }
